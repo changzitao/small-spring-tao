964b3675449c518dc15c36009b696fdbd872acdf
chang <fy1966426197@126.com>
2023-03-16 17:59:39 +0800

    aaa

---
+++ small-spring-step1/src/main/java/tao/chang/springframework/BeanFactory.java
@@ -0,0 +1,13 @@
+package tao.chang.springframework;
+import java.util.HashMap;
+import java.util.Map;
+public class BeanFactory {
+    private Map<String ,BeanDefinition> beanDefinitionMap = new HashMap<>();
+    public Object getBean(String  name){
+        return beanDefinitionMap.get(name).getBean();
+    }
+
+    public void registerBeanDefinition(String name,BeanDefinition beanDefinition){
+        beanDefinitionMap.put(name,beanDefinition);
+    }
+}
